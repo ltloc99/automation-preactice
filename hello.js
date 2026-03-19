203c353081d2eddcbe8d116a59cf1e1c226f3b58
loclt99 <loclt9991@gmail.com>
2026-03-19 23:25:38 +0700

    Add sayGoodbye function

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
 function sayHello(name) {
     return "Hello, "+ name + "!";
 }
+function sayGoodbye(name) {
+ return "Goodbye, " + name + "!";
+}
 console.log(sayHello("Automation Tester"));
+console.log(sayGoodbye("Manual Testing"));

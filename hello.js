2575ff30b52ac9de0bcdfa728308f8bcff95e717
loclt99 <loclt9991@gmail.com>
2026-03-20 22:53:54 +0700

    commit hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,1 @@
-function sayHello(name) {
-    return "Hello, "+ name + "!";
-}
-function sayGoodbye(name) {
- return "Goodbye, " + name + "!";
-}
-console.log(sayHello("Automation Tester"));
-console.log(sayGoodbye("Manual Testing"));
+Hello World 
